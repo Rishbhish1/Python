620ab7d61fd9d2f9bfeeda7b57882e7a716cf4a8
Rishabh Sehgal <x.rishxbh.x@gmail.com>
2024-07-20 20:03:04 +0530

    Comments

--- dataVisDashboard.py
+++ dataVisDashboard.py
@@ -1,12 +1,12 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
+import matplotlib.pyplot as plt                     #  library for creating static, animated, and interactive visualizations 
+import seaborn as sns                               # library based on Matplotlib that provides a high-level interface for drawing attractive statistical graphics
+import pandas as pd                                 # data manipulation
 
 # Load the Iris dataset
-df = sns.load_dataset('iris')
+df = sns.load_dataset('iris')                       # add file path if needed in '' (csv file)
 
 # Create a figure with multiple subplots
-fig, axes = plt.subplots(2, 2, figsize=(15, 10))
+fig, axes = plt.subplots(2, 2, figsize=(15, 10))    # 2x2 grid of subplots, size of the entire figure to 15 inches by 10 inches.
 
 # !Plot 1: Scatter plot of Sepal Length vs Sepal Width
 sns.scatterplot(data=df, x='sepal_length', y='sepal_width', hue='species', ax=axes[0, 0])
